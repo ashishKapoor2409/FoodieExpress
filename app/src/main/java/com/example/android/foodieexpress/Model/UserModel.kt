bd29615eb8b17f7711fd740c39f803598873efd4
Ashish Kapoor <kapoorashish810@gmail.com>
2022-03-20 01:43:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/foodieexpress/Model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.example.android.foodieexpress.Model
+
+class UserModel {
+    var uid: String?=null
+    var name:String?= null
+    var address:String? = null
+    var phone:String?=null
+
+    constructor(){ }
+}
