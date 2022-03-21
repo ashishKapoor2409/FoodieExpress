9313ab8e0eccfaf2f8b1832d97edadfd47dea134
Ashish Kapoor <kapoorashish810@gmail.com>
2022-03-21 19:26:40 +0530

    Category Screen and Animation for RecyclerView added

--- app/src/main/java/com/example/android/foodieexpress/Common/Common.kt
+++ app/src/main/java/com/example/android/foodieexpress/Common/Common.kt
@@ -3,6 +3,9 @@ package com.example.android.foodieexpress.Common
 import com.example.android.foodieexpress.Model.UserModel
 
 object Common {
+    val CATEGORY_REF: String = "Category"
+    val FULL_WIDTH_COLUMN: Int = 1
+    val DEFAULT_COLUMN_COUNT: Int = 0
     val BEST_DEALS_REF: String = "BestDeals"
     val POPULAR_REF: String = "MostPopular"
     val USER_REFERENCE = "Users"
